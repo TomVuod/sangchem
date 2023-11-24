a3d8d2c1e5b08c3316d660411a62d77adb8348ee
@tomwlo <tomwlo@gmail.com>
2023-11-24 22:35:50 +0100

    spe718 cies identity index

--- R/species_identity.R
+++ R/species_identity.R
@@ -4,7 +4,7 @@
 calculate_SII <- function(MS_data, prediction_model=species_prediction_model,
                           PCA_res = PCA_species_discr){
   peak_prop <- peak_proportions_table(MS_data)
-  peak_prop <- peak_prop[,peak_prop %in% rownames(PCA_res$rotation)]
+  peak_prop <- peak_prop[,colnames(peak_prop) %in% rownames(PCA_res$rotation)]
   peak_prop <- peak_prop/rowSums(peak_prop)
   missing_variables <- setdiff(rownames(PCA_res$rotation), colnames(peak_prop))
   peak_prop <- cbind(peak_prop, matrix(0, nrow=nrow(peak_prop), ncol=length(missing_variables),
@@ -14,21 +14,15 @@ calculate_SII <- function(MS_data, prediction_model=species_prediction_model,
   # PCA transformation using data from training set
   peak_prop <- t(apply(peak_prop, 1, function(x) x-PCA_res$center))
   peak_prop <- peak_prop%*%PCA_res$rotation
-  predicted <- predict(species_model, peak_prop)$predict
+  predicted <- predict(prediction_model, peak_prop)$predict
   cbind(data.frame(chromatogram_ID=as.numeric(dimnames(predicted)[[1]])),
-        predicted_species=predicted[,1,species_model$ncomp])
+        predicted_species=predicted[,2,prediction_model$ncomp])
 }
 
-
-
-
-predict_species<-function(chrIDs, model=plsda_CBII, ncomp_included=3){
-  if (any(is.na(chrIDs))) {
-    warning("NA value of chromatogram ID passed in")
-    return(NA)
-  }
-  data<-pca_projection(chrIDs)
-  predicted<-predict(model,data)$predict
-  cbind(data.frame(chromatogram_ID=as.numeric(dimnames(predicted)[[1]])),
-        predicted_species=predicted[,1,ncomp_included])
+#' @export
+find_nestmates_SII <- function(chr_IDs, species_indices, heterospecific=TRUE){
+  lapply(chr_IDs, function(x) find_sample_nestmate(x, heterospecifc=heterospecific)) %>%
+  lapply(function(x) species_indices[species_indices$chromatogram_ID %in% x,"predicted_species"]) %>%
+  lapply(function(x) mean(x, na.rm=TRUE)) %>%
+  unlist()
 }
