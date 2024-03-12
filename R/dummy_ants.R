e0588218fcba62abde9efb17cb1ba25935293927
@tomwlo <tomwlo@gmail.com>
2024-03-12 22:51:29 +0100

    bug fixes

--- R/dummy_ants.R
+++ R/dummy_ants.R
@@ -6,17 +6,21 @@ recognize_groups<-function(sample_data=separation_experiment_data){
   temp
 }
 
-determine_donor_chr_ID<-function(chrID,species_,donor_mapping_=donor_mapping,
-                                 sep_data=separation_experiment_data){
-  col<-as.character(filter(sep_data,chromatogram_ID==chrID)$colony)
-  year_<-filter(sep_data,chromatogram_ID==chrID)$year
-  if((species_=="F. fusca")&(col=="SD18-5")&(year_==2020)){
+determine_donor_chr_ID<-function(chrID, species, donor_mapping,
+                                 sep_data){
+  col <- as.character(filter(sep_data,chromatogram_ID==chrID)$colony)
+  year <- filter(sep_data,chromatogram_ID==chrID)$year
+  if((species=="F. fusca")&(col=="SD18-5")&(year==2020)){
     pupa_1<-filter(sep_data,chromatogram_ID==chrID)$pupa_1
     if(pupa_1<as.POSIXct("2020-08-01",format="%Y-%m-%d"))
       return(293)
     else return(294)
   }
-  filter(donor_mapping_,species==species_,colony==col,year==year_)$chromatogram_ID
+  species_ = species
+  year_ = year
+  donor_chr_ID <- filter(donor_mapping,species==species_,colony==col,year==year_)$chromatogram_ID
+  if(length(donor_chr_ID)==0) return(NA)
+  else donor_chr_ID
 }
 
 # add control sample if present; if not removes the treatment sample
@@ -39,13 +43,15 @@ select_group_with_control<-function(sample_data,treatment_id){
 
 # calculate distance to the colony which was used as donor of CHCs applicated onto the dummy ants
 # the same is done for corresponding control samples
-calculate_distance_to_donor<-function(treatment_id,sample_data){
+calculate_distance_to_donor<-function(treatment_id, sample_data, MS_data, donor_mapping){
   sample_data<-recognize_groups(sample_data)
   sample_data<-select_group_with_control(sample_data,treatment_id)
   treatments<-5:8
   species<-rep(c("F. fusca", "F. sanguinea"),each=2)[treatments==treatment_id]
-  sample_data$donor_id<-mapply(function(x) determine_donor_chr_ID(x,species), data$chromatogram_ID)
-  sample_data$distance<-mapply(function(x,y) pair_distance(c(x,y)), sample_data$chromatogram_ID, sample_data$donor_id)
+  sample_data$donor_id<-mapply(function(x) determine_donor_chr_ID(x, species,
+                                                                  sep_data = sample_data, donor_mapping=donor_mapping), sample_data$chromatogram_ID)
+  sample_data <- filter(sample_data, !is.na(donor_id))
+  sample_data$distance<-mapply(function(x,y) pair_distance(x,y, MS_data), sample_data$chromatogram_ID, sample_data$donor_id)
   sample_data %>% group_by(colony,year,group,treatment_id) %>% summarise(mean_dist=mean(distance))
 }
 
@@ -54,11 +60,15 @@ calculate_distance_to_donor<-function(treatment_id,sample_data){
 #' Test of the effect of dummy ant on the CHC profile of separated workers
 #'
 #' @param treatment_id a numeric indicating treatment ID to be tested on the effect of
-#' @param sample_data a data frame describing samples used in separation experiment; rerieved with `data("separation_experiment_data")`
 #' dummy ant.
+#' @param sample_data a data frame describing samples used in separation experiment; retrieved with `data("separation_experiment_data")`
+#' @param MS_data a data frame with the results of the chromatogram peaks integration; ; retrieved with `data("mass_spectra_data")`
+#' @param donor_mapping a data frame providing sample ID mapping between control and treatment samples
+#' in the dummy ant experiment
 #' @export
-test_dummy_ant_effect <- function(treatment_id, sample_data){
-  distances_to_donor<-calculate_distance_to_donor(treatment_id = treatment_id, sample_data = sample_data)
+test_dummy_ant_effect <- function(treatment_id, sample_data, MS_data, donor_mapping){
+  distances_to_donor<-calculate_distance_to_donor(treatment_id = treatment_id, sample_data = sample_data,
+                                                  donor_mapping=donor_mapping, MS_data=MS_data)
   splitted<-split(distances_to_donor, distances_to_donor$treatment_id)
   to_test<-full_join(splitted[[1]], splitted[[2]], by=c("group","colony","year"))
   print(to_test)
