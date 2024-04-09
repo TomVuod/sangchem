27edf547c85c027170eb089be8516ee4b5ad44e2
@tomwlo <tomwlo@gmail.com>
2024-04-09 07:47:19 +0200

    figures update

--- scripts/Figures.R
+++ scripts/Figures.R
@@ -2,66 +2,63 @@ library(ggrepel)
 library(ggplot2)
 library(dplyr)
 library(sangchem)
+library(forcats)
+library(ggpubr)
+library(rstatix)
+
+
+
 load_globals()
 data("development_data")
-df <- filter(development_data, caste=="worker",!is.na(head_width), !remarks %in% c("aggression actor", "aggression target", "queenless colony"), species=="F. sanguinea") %>%
-  select(colony, census_date, chromatogram_ID,
-       callow, mass, head_width, species, sang_prop) %>%
-  mutate(mass=mass/(head_width/1300)^2)
-
-df1 <- filter(df, species=="F. sanguinea", callow==1) %>%
-  select(colony, census_date, chromatogram_ID, callow, mass, sang_prop) %>%
-  group_by(colony,census_date) %>%
-  {function(x) {x$pos<-as.factor(group_indices(x)); ungroup(x)}}()
-
-# duplicated samples should converge, thus they should have the same position on x axis
-# each level of the factor correspond to the same colony/sampling date combination
-levels(df1$pos)<-seq(-0.15,0.15,length.out = length(levels(df1$pos)))
-df1$pos <- as.numeric(as.character(df1$pos))
-df1$callow_ID <- df1$chromatogram_ID # new column name not to be duplicated by joining with mature ants df
+paired_observations <- function(group_1, group_2, values_column, group_var="callow", peak_subset=NULL){
+  df <- filter(development_data, caste=="worker",!is.na(head_width), !remarks %in% c("aggression actor",
+                                                                                     "aggression target",
+                                                                                     "queenless colony"),
+               species=="F. sanguinea") %>%
+    select(colony, census_date, chromatogram_ID,
+           callow, mass, head_width, species, sang_prop)
+  if(values_column=="mass")
+    df <- mutate(df, mass=mass/(head_width/1300)^2)
+  else
+    df$proportion <- subsample_proportion(peak_subset, df$chromatogram_ID)
 
 
-filter(df, species=="F. sanguinea", callow==0) %>%
-  select(colony, census_date, chromatogram_ID, callow, mass, sang_prop) -> df2
-# pair with callow ants form the same colony and sampled at the same time
-df2<-left_join(df2, select(df1, pos, colony, census_date, callow_ID), relationship="many-to-many")
-# drop unpaired samples
-df2 <- df2[!is.na(df2$callow_ID),]
-df2$ID<-1:nrow(df2) # make rows unique before joining to df1
-
-# transer pair ID to callow data frame
-df1<-left_join(df1,select(df2, ID,callow_ID))
-df<-rbind(df1, df2)
-df$mature=df$callow==0
-
-ggplot(aes(x=as.numeric(mature), y=mass , group=mature), data=df)+
-  geom_boxplot(col="#1f4a24",
-                fill="#1f4a2466", width=0.6, lwd=1, outlier.shape=NA)+
- geom_point(aes(x=as.numeric(mature)+pos, y=mass, fill=sang_prop),shape=21,cex=3.4,color="black")+
-  geom_line(aes(x=as.numeric(mature)+pos, y=mass, group=ID), col="#444444")+
-  scale_y_continuous(trans="log2", breaks=2^(0:4))+
-  scale_x_continuous(breaks = c(0,1), labels = c("Callow ants", "Mature ants"))+
-  scale_fill_gradient("Proportion of\nF. sanguinea ants", low="#555C58", high="#BBEA5E")+
-  xlab("F. sanguinea workers")+
-  ylab(expression(paste("Cucitular hydrocarbon mass per individual [",mu,"g]")))+
-  theme(axis.text.x = element_text(size = 15))+
-  theme(axis.text.y = element_text(size = 15))+
-  theme(axis.title=element_text(size=18))+
-  theme(legend.title=element_text(size=14),
-        legend.text=element_text(size=13))+
-  theme(panel.background = element_rect(fill="white"),
-        axis.line = element_line(size = 0.5, linetype = "solid",
-                                 colour = "black"))
+  df1 <- filter(df, species==!!group_1$species, callow==group_1$callow) %>%
+    select(colony, census_date, chromatogram_ID, callow, !!group_var, sang_prop, !!values_column)
 
 
-df$mass <- df$mass*subsample_proportion(peaks_callow, df$chromatogram_ID)
+  # duplicated samples should converge, thus they should have the same position on x axis
+  # each level of the factor correspond to the same colony/sampling date combination
+  df1$point_x_pos <- fct_cross(df1$colony, as.character(df1$census_date))
+  levels(df1$point_x_pos) <- sample(seq(-0.15,0.15,length.out = length(levels(df1$point_x_pos))))
+  df1$point_x_pos <- as.numeric(as.character(df1$point_x_pos))
 
 
-ggplot(aes(x=as.numeric(mature), y=mass , group=mature), data=df)+
+  filter(df, species==!!group_2$species, callow==group_2$callow) %>%
+    select(colony, census_date, chromatogram_ID, callow, !!group_var, sang_prop, !!values_column) -> df2
+  # pair with callow ants form the same colony and sampled at the same time
+  df <- inner_join(df1, df2, relationship="many-to-many", by = c("colony", "census_date", "sang_prop"))
+  # add pair ID
+  df$ID <- as.numeric(fct_cross(as.character(df$chromatogram_ID.x), as.character(df$chromatogram_ID.y)))
+  df1 <- unique(df[,grep(".*\\.y$", colnames(df), invert = TRUE)])
+  colnames(df1) <- gsub("\\.x", "",colnames(df1))
+  df2 <- unique(df[,grep(".*\\.x$", colnames(df), invert = TRUE)])
+  colnames(df2) <- gsub("\\.y", "",colnames(df2))
+  df<-rbind(df1, df2[,colnames(df1)])
+  df[,group_var] <- df[,group_var]==group_1[[group_var]]
+  return(df)
+}
+
+df <- paired_observations(list(species = "F. sanguinea", callow = 0),
+                            group_2=list(species = "F. sanguinea", callow = 1),
+                            values_column="mass", group_var="callow")
+df$callow <- as.numeric(df$callow)
+
+p1 <- ggplot(aes(x=callow, y=mass, group=callow), data=df)+
   geom_boxplot(col="#1f4a24",
                fill="#1f4a2466", width=0.6, lwd=1, outlier.shape=NA)+
-  geom_point(aes(x=as.numeric(mature)+pos, y=mass, fill=sang_prop),shape=21,cex=3.4,color="black")+
-  #geom_line(aes(x=as.numeric(mature)+pos, y=mass, group=ID), col="#444444")+
+  geom_point(aes(x=callow+point_x_pos, y=mass, fill=sang_prop), shape=21, cex=3.4, color="black")+
+  geom_line(aes(x=callow+point_x_pos, y=mass, group=ID), col="#444444")+
   scale_y_continuous(trans="log2", breaks=2^(0:4))+
   scale_x_continuous(breaks = c(0,1), labels = c("Callow ants", "Mature ants"))+
   scale_fill_gradient("Proportion of\nF. sanguinea ants", low="#555C58", high="#BBEA5E")+
@@ -74,6 +71,49 @@ ggplot(aes(x=as.numeric(mature), y=mass , group=mature), data=df)+
         legend.text=element_text(size=13))+
   theme(panel.background = element_rect(fill="white"),
         axis.line = element_line(size = 0.5, linetype = "solid",
-                                 colour = "black"))
+                                 colour = "black"),
+        legend.position = "none")+
+  geom_signif(
+    y_position = log2(max(df[["mass"]])*1.1), xmin = 0, xmax = 1,
+    annotation = 0.0000305, tip_length = 0.02
+  )
+
+
+df <- paired_observations(list(species = "F. sanguinea", callow = 0),
+                          group_2=list(species = "F. sanguinea", callow = 1),
+                          peak_subset = peaks_callow,
+                          values_column="proportion", group_var="callow"
+)
+
+
+df$mature<- as.numeric(df$callow)
+p2 <- ggplot(aes(x=as.numeric(callow), y=proportion , group=mature), data=df)+
+  geom_boxplot(col="#1f4a24",
+               fill="#1f4a2466", width=0.6, lwd=1, outlier.shape=NA)+
+  geom_point(aes(x=as.numeric(callow)+point_x_pos, y=proportion, fill=sang_prop),shape=21,cex=3.4,color="black")+
+  #geom_line(aes(x=as.numeric(mature)+pos, y=mass, group=ID), col="#444444")+
+  #scale_y_continuous(trans="log2", breaks=2^(0:4))+
+  scale_x_continuous(breaks = c(0,1), labels = c("Callow ants", "Mature ants"))+
+  scale_fill_gradient("Proportion of\nF. sanguinea ants", low="#555C58", high="#BBEA5E")+
+  xlab("F. sanguinea workers")+
+  ylab(expression(paste("Cucitular hydrocarbon mass per individual 000 [",mu,"g]")))+
+  theme(axis.text.x = element_text(size = 15))+
+  theme(axis.text.y = element_text(size = 15))+
+  theme(axis.title=element_text(size=18))+
+  theme(legend.title=element_text(size=14),
+        legend.text=element_text(size=13))+
+  theme(panel.background = element_rect(fill="white"),
+        axis.line = element_line(size = 0.5, linetype = "solid",
+                                 colour = "black"))+
+  geom_signif(
+    y_position = max(df[["proportion"]])*1.1, xmin = 0, xmax = 1,
+    annotation = 0.000153, tip_length = 0.02
+  )
+
+
+
+ggarrange(p1, p2, ncol=2, labels=c("A", "B"))
+
+
 
 
