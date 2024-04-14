6d9fb7d4863c3f31fc95abfd036679f0a9d0308c
Tomek <tomwlo@gmail.com>
2024-04-14 10:17:49 +0200

    cross validation

--- scripts/DA_2_groups_random.R
+++ scripts/DA_2_groups_random.R
@@ -3,45 +3,6 @@ devtools::load_all(".")
 args = commandArgs(trailingOnly=TRUE)
 library(mixOmics)
 library(dplyr)
-calculate_AUC <- function(x,y){
-  y <- y[order(x)]
-  x <- x[order(x)]
-  non_unique_x_ind <- which(duplicated(x))
-  non_unique_x_ind <- sort(unique(c(non_unique_x_ind, non_unique_x_ind-1)))
-  y[non_unique_x_ind] <- sort(y[non_unique_x_ind])
-  if(any(diff(y)<0)) stop("y should be non-decreasing function of x")
-  x_intervals <- diff(x)
-  pair_mean_y <- (y[1:(length(y)-1)] + y[2:length(y)])/2
-  sum(x_intervals*pair_mean_y)
-}
-
-
-calculate_accuracy_metrics <- function(values, labels){
-  labels <- labels[order(values)]
-  values <- sort(values)
-  res_list <- list()
-  all_combinations <- data.frame(threshold_reached=as.logical(c(1,1,0,0)), category = as.logical(c(1,0,1,0)))
-  confusion_matrix <- data.frame(threshold_reached =0, category = as.numeric(labels))
-  confusion_matrix <- rbind(all_combinations, confusion_matrix)
-  tab <- table(confusion_matrix)
-  tab <- tab - 1
-  res_list[[1]] <- c(TP = tab[2,2], FP = tab[2, 1], TN = tab[1, 1],
-                     FN = tab[1, 2], cutoff = NA)
-  for(i in seq_along(values)){
-    threshold_reached <- values >= values[i]
-    confusion_matrix <- data.frame(threshold_reached =threshold_reached, category = as.numeric(labels))
-    confusion_matrix <- rbind(all_combinations, confusion_matrix)
-    tab <- table(confusion_matrix)
-    # account for adding one observation for combination
-    tab <- tab - 1
-    res_list[[i+1]] <- c(TP = tab[2,2], FP = tab[2, 1], TN = tab[1, 1],
-                         FN = tab[1, 2], cutoff = values[i])
-  }
-  res <- as.data.frame(do.call(rbind, res_list))
-  TPR <- res$TP/(res$TP + res$FN)
-  FPR <- res$FP/(res$FP + res$TN)
-  list(TPR = TPR, FPR = FPR, cutoff = res$cutoff, confusion_matrix_data = res)
-}
 # load data
 data("development_data")
 data("mass_spectra_data")
@@ -110,15 +71,22 @@ if(length(results$Y)>0) Y <- results$Y[[length(results$Y)]]
                                        keepX=discr_analysis_callows_tuned$choice.keepX,
                                        ncomp=n_comp,
                                        scale = FALSE)
-  predicted_species <- calculate_SII(peak_prop, discr_analysis_callows_res, PCA_callow_discr, predicted_category=2,
-                                     multilevel=DA_data$group)$predicted_species
+
+  perf.discr_analysis_callows_res <- perf(discr_analysis_callows_res,
+                                     validation = "Mfold",
+                                     folds = 4,
+                                     progressBar = FALSE,
+                                     nrepeat = 10,
+                                     scale = FALSE,
+                                     cpus=18)
+
   #curve_data <- calculate_accuracy_metrics(predicted_species[Y>0], Y[Y>0]==2)
   #AUC <- calculate_AUC(curve_data$FPR, curve_data$TPR)
   #results$AUC <- c(results$AUC, AUC)
   results$random.seed <- .Random.seed
   results$Y <- c(results$Y, list(Y))
-  results$predictions <- c(results$predictions, list(predicted_species))
-  saveRDS(results, "random_labels_2_groups.rds")}, error=function(cond) 0)
+  results$predictions <- c(results$predictions, perf.discr_analysis_callows_res$predict)
+  saveRDS(results, "random_labels_2_groups.rds")}, error=function(cond) print(cond))
   if(length(results$predictions)>=as.numeric(args[1])) break
 }
 
