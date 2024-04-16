bb47e80c57d260d7e5a5b86022687561f7bed69d
Tomek <tomwlo@gmail.com>
2024-04-16 11:57:59 +0200

    add necessary functions

--- scripts/DA_2_groups_random.R
+++ scripts/DA_2_groups_random.R
@@ -7,6 +7,86 @@ library(dplyr)
 data("development_data")
 data("mass_spectra_data")
 
+calculate_AUC <- function(x,y){
+  y <- y[order(x)]
+  x <- x[order(x)]
+  non_unique_x_ind <- which(duplicated(x))
+  non_unique_x_ind <- sort(unique(c(non_unique_x_ind, non_unique_x_ind-1)))
+  y[non_unique_x_ind] <- sort(y[non_unique_x_ind])
+  if(any(diff(y)<0)) stop("y should be non-decreasing function of x")
+  x_intervals <- diff(x)
+  pair_mean_y <- (y[1:(length(y)-1)] + y[2:length(y)])/2
+  sum(x_intervals*pair_mean_y)
+}
+
+
+calculate_accuracy_metrics <- function(values, labels){
+  labels <- labels[order(values)]
+  values <- sort(values)
+  res_list <- list()
+  all_combinations <- data.frame(threshold_reached=as.logical(c(1,1,0,0)), category = as.logical(c(1,0,1,0)))
+  confusion_matrix <- data.frame(threshold_reached =0, category = as.numeric(labels))
+  confusion_matrix <- rbind(all_combinations, confusion_matrix)
+  tab <- table(confusion_matrix)
+  tab <- tab - 1
+  res_list[[1]] <- c(TP = tab[2,2], FP = tab[2, 1], TN = tab[1, 1],
+                     FN = tab[1, 2], cutoff = NA)
+  for(i in seq_along(values)){
+    threshold_reached <- values >= values[i]
+    confusion_matrix <- data.frame(threshold_reached =threshold_reached, category = as.numeric(labels))
+    confusion_matrix <- rbind(all_combinations, confusion_matrix)
+    tab <- table(confusion_matrix)
+    # account for adding one observation for combination
+    tab <- tab - 1
+    res_list[[i+1]] <- c(TP = tab[2,2], FP = tab[2, 1], TN = tab[1, 1],
+                         FN = tab[1, 2], cutoff = values[i])
+  }
+  res <- as.data.frame(do.call(rbind, res_list))
+  TPR <- res$TP/(res$TP + res$FN)
+  FPR <- res$FP/(res$FP + res$TN)
+  list(TPR = TPR, FPR = FPR, cutoff = res$cutoff, confusion_matrix_data = res)
+}
+
+calculate_AUC <- function(x,y){
+  y <- y[order(x)]
+  x <- x[order(x)]
+  non_unique_x_ind <- which(duplicated(x))
+  non_unique_x_ind <- sort(unique(c(non_unique_x_ind, non_unique_x_ind-1)))
+  y[non_unique_x_ind] <- sort(y[non_unique_x_ind])
+  if(any(diff(y)<0)) stop("y should be non-decreasing function of x")
+  x_intervals <- diff(x)
+  pair_mean_y <- (y[1:(length(y)-1)] + y[2:length(y)])/2
+  sum(x_intervals*pair_mean_y)
+}
+
+
+calculate_accuracy_metrics <- function(values, labels){
+  labels <- labels[order(values)]
+  values <- sort(values)
+  res_list <- list()
+  all_combinations <- data.frame(threshold_reached=as.logical(c(1,1,0,0)), category = as.logical(c(1,0,1,0)))
+  confusion_matrix <- data.frame(threshold_reached =0, category = as.numeric(labels))
+  confusion_matrix <- rbind(all_combinations, confusion_matrix)
+  tab <- table(confusion_matrix)
+  tab <- tab - 1
+  res_list[[1]] <- c(TP = tab[2,2], FP = tab[2, 1], TN = tab[1, 1],
+                     FN = tab[1, 2], cutoff = NA)
+  for(i in seq_along(values)){
+    threshold_reached <- values >= values[i]
+    confusion_matrix <- data.frame(threshold_reached =threshold_reached, category = as.numeric(labels))
+    confusion_matrix <- rbind(all_combinations, confusion_matrix)
+    tab <- table(confusion_matrix)
+    # account for adding one observation for combination
+    tab <- tab - 1
+    res_list[[i+1]] <- c(TP = tab[2,2], FP = tab[2, 1], TN = tab[1, 1],
+                         FN = tab[1, 2], cutoff = values[i])
+  }
+  res <- as.data.frame(do.call(rbind, res_list))
+  TPR <- res$TP/(res$TP + res$FN)
+  FPR <- res$FP/(res$FP + res$TN)
+  list(TPR = TPR, FPR = FPR, cutoff = res$cutoff, confusion_matrix_data = res)
+}
+
 DA_data <- dplyr::filter(development_data, caste=="worker", !remarks %in% c("aggression actor", "aggression target", "queenless colony"),
                          species == "F. sanguinea") %>%
   dplyr::select(colony, species, callow, census_date, chromatogram_ID, sang_prop) %>%
