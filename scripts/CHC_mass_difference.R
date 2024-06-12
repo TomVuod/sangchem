731e8df6aa9bd4f6ed4b70cdefb07f0c537263db
Tomek <tomwlo@gmail.com>
2024-06-12 20:38:51 +0200

    parallel computing

--- scripts/CHC_mass_difference.R
+++ scripts/CHC_mass_difference.R
@@ -3,6 +3,7 @@
   library(lme4)
   library(lmerTest)
   library(DHARMa)
+  library(BiocParallel)
 data("mass_spectra_data")
 data("development_data")
 dev_samples <- filter(development_data, caste=="worker",!is.na(head_width), !remarks %in% c("aggression actor", "aggression target", "queenless colony"), callow==0) %>%
@@ -14,15 +15,18 @@ lm_model <- lmer(I(log(corrected_mass)) ~ sang_prop  + (1|colony) +(1 | colony:c
 
 
 simulation_number=10^6
-set.seed(4512)
-sang_fam_simulated<-bootMer(lm_model,function(x) predict(x,data.frame(sang_prop=1),re.form=NA),nsim=simulation_number)$t
-pred_values_sang <- exp(sang_fam_simulated)
-
+seeds <- (1:1000)*17
+predict_values <- function(n, model, sang_prop, seeds){
+  set.seed(seeds[n])
+  exp(bootMer(model,function(x) predict(x,data.frame(sang_prop=sang_prop),re.form=NA),nsim=10^3)$t)
+}
+pred_values_sang <- blapply(1:1000, predict_values, model = lm_model, sang_prop = 1, seeds = seeds, BPPARAM = MulticoreParam())
+pred_values_sang <- do.call(c, pred_values_sang)
 
 model_input <- dev_samples[dev_samples$species=="F. fusca",]
 lm_model <- lmer(I(log(corrected_mass)) ~ sang_prop  + (1|colony), data=model_input)
-fus_fam_simulated<-bootMer(lm_model,function(x) predict(x,data.frame(sang_prop=0),re.form=NA),nsim=simulation_number)$t
-pred_values_fusca <- exp(fus_fam_simulated)
+pred_values_fusca <- blapply(1:1000, predict_values, model = lm_model, sang_prop = 0, seeds = seeds, BPPARAM = MulticoreParam())
+pred_values_fusca <- do.call(c, pred_values_fusca)
 
-saveRDS(list(pred_values_sang, pred_values_fusca), "predicted_values.rds")
+saveRDS(list(pred_values_sang=pred_values_sang, pred_values_fusca=pred_values_fusca), "predicted_values.rds")
 
