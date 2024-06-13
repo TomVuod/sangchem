9f6c2405b8d02e2088cfbb9f4d248961eb7135d8
Tomek <tomwlo@gmail.com>
2024-06-13 21:46:47 +0200

    validation results

---
+++ scripts/process_validation_data.R
@@ -0,0 +1,12 @@
+# process cross-validation on high-performance computing nodes
+# merge files with partial results into single ones
+
+AUC_basic <- c()
+for(i in 1:10) AUC_basic <- c(AUC_basic, readRDS(paste0("true_labels_3_groups_", i, ".rds"))$AUC[1:100])
+
+AUC_random <- c()
+for(i in 1:10) AUC_random <- c(AUC_random, readRDS(paste0("random_labels_3_groups_", i, ".rds"))$AUC[1:100])
+
+
+discrimination_model_validation <- list(AUC_observed = AUC_basic, AUC_null = AUC_random)
+usethis::use_data(discrimination_model_validation)
