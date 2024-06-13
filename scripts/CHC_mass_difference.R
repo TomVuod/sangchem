3ccb58c26018d653d5ac1824a3be84bb68e10649
Tomasz Włodarczyk <tomwlo@gmail.com>
2024-06-13 09:30:19 +0200

    correct output path and function name

--- scripts/CHC_mass_difference.R
+++ scripts/CHC_mass_difference.R
@@ -20,13 +20,13 @@ predict_values <- function(n, model, sang_prop, seeds){
   set.seed(seeds[n])
   exp(bootMer(model,function(x) predict(x,data.frame(sang_prop=sang_prop),re.form=NA),nsim=10^3)$t)
 }
-pred_values_sang <- blapply(1:1000, predict_values, model = lm_model, sang_prop = 1, seeds = seeds, BPPARAM = MulticoreParam())
+pred_values_sang <- bplapply(1:10000, predict_values, model = lm_model, sang_prop = 1, seeds = seeds, BPPARAM = MulticoreParam())
 pred_values_sang <- do.call(c, pred_values_sang)
 
 model_input <- dev_samples[dev_samples$species=="F. fusca",]
 lm_model <- lmer(I(log(corrected_mass)) ~ sang_prop  + (1|colony), data=model_input)
-pred_values_fusca <- blapply(1:1000, predict_values, model = lm_model, sang_prop = 0, seeds = seeds, BPPARAM = MulticoreParam())
+pred_values_fusca <- bplapply(1:10000, predict_values, model = lm_model, sang_prop = 0, seeds = seeds, BPPARAM = MulticoreParam())
 pred_values_fusca <- do.call(c, pred_values_fusca)
 
-saveRDS(list(pred_values_sang=pred_values_sang, pred_values_fusca=pred_values_fusca), "predicted_values.rds")
+saveRDS(list(pred_values_sang=pred_values_sang, pred_values_fusca=pred_values_fusca), "/home/t.wlodarczyk/chemical_ecology/sangchem/predicted_values.rds")
 
