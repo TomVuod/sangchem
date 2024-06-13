c2fe14ef8b9476a9b001c61bd2f76214880f1019
Tomek <tomwlo@gmail.com>
2024-06-13 09:49:13 +0200

    preserve seeds

--- scripts/CHC_mass_difference.R
+++ scripts/CHC_mass_difference.R
@@ -9,7 +9,19 @@ data("development_data")
 dev_samples <- filter(development_data, caste=="worker",!is.na(head_width), !remarks %in% c("aggression actor", "aggression target", "queenless colony"), callow==0) %>%
   dplyr::select(chromatogram_ID, species, colony, sang_prop, head_width, mass, census_date) %>%
   mutate(corrected_mass=mass/head_width^2*10^6)
-
+  rseeds <- list()
+  for(seed_int in (1:1000)*17){
+    set.seed(seed_int)
+    rseeds <- c(rseeds, list(.Random.seed))
+  }
+  seeds_fusca <- seeds_sang <- rseeds
+if(file.exists("/home/t.wlodarczyk/chemical_ecology/sangchem/predicted_values.rds")){
+  predicted_values <- readRDS("/home/t.wlodarczyk/chemical_ecology/sangchem/predicted_values.rds")
+  pred_values_sang <- predicted_values$pred_values_sang
+  pred_values_fusca <- predicted_values$pred_values_fusca
+  seeds_sang <- seeds_sang
+  seeds_fusca <- seeds_fusca
+}
 model_input <- dev_samples[dev_samples$species=="F. sanguinea",]
 lm_model <- lmer(I(log(corrected_mass)) ~ sang_prop  + (1|colony) +(1 | colony:census_date), data=model_input)
 
@@ -18,15 +30,26 @@ simulation_number=10^6
 seeds <- (1:1000)*17
 predict_values <- function(n, model, sang_prop, seeds){
   set.seed(seeds[n])
-  exp(bootMer(model,function(x) predict(x,data.frame(sang_prop=sang_prop),re.form=NA),nsim=10^3)$t)
+  list(t=exp(bootMer(model,function(x) predict(x,data.frame(sang_prop=sang_prop),re.form=NA),nsim=10^4)$t),
+       rseed = .Random.seed)
 }
-pred_values_sang <- bplapply(1:10000, predict_values, model = lm_model, sang_prop = 1, seeds = seeds, BPPARAM = MulticoreParam())
-pred_values_sang <- do.call(c, pred_values_sang)
+pred_values_sang <- bplapply(1:1000, predict_values, model = lm_model, sang_prop = 1, seeds = seeds, BPPARAM = MulticoreParam())
+res_sang <- c()
+seeds_sang <- list()
+for(i in seq_along(pred_values_sang)){
+  res_sang <- c(res_sang, pred_values_sang$t)
+  seeds_sang <- c(seeds_sang, pred_values_sang[2])
+}
 
 model_input <- dev_samples[dev_samples$species=="F. fusca",]
 lm_model <- lmer(I(log(corrected_mass)) ~ sang_prop  + (1|colony), data=model_input)
-pred_values_fusca <- bplapply(1:10000, predict_values, model = lm_model, sang_prop = 0, seeds = seeds, BPPARAM = MulticoreParam())
-pred_values_fusca <- do.call(c, pred_values_fusca)
+pred_values_fusca <- bplapply(1:1000, predict_values, model = lm_model, sang_prop = 0, seeds = seeds, BPPARAM = MulticoreParam())
+res_fusca <- c()
+seeds_fusca <- list()
+for(i in seq_along(pred_values_fusca)){
+  res_sang <- c(res_sang, pred_values_fusca$t)
+  seeds_sang <- c(seeds_sang, pred_values_fusca[2])
+}
 
-saveRDS(list(pred_values_sang=pred_values_sang, pred_values_fusca=pred_values_fusca), "/home/t.wlodarczyk/chemical_ecology/sangchem/predicted_values.rds")
+saveRDS(list(pred_values_sang=res_sang, pred_values_fusca=res_fusca, seeds_sang = seeds_sang, seeds_fusca = seeds_fusca), "/home/t.wlodarczyk/chemical_ecology/sangchem/predicted_values.rds")
 
