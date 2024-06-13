b6dd00ecec408187503ea55552ca868d2cdca195
@tomwlo <tomwlo@gmail.com>
2024-06-13 21:27:16 +0200

    bug fix: include previous results

--- scripts/CHC_mass_difference.R
+++ scripts/CHC_mass_difference.R
@@ -15,10 +15,11 @@ dev_samples <- filter(development_data, caste=="worker",!is.na(head_width), !rem
     rseeds <- c(rseeds, list(.Random.seed))
   }
   seeds_fusca <- seeds_sang <- rseeds
+  res_sang <- res_fusca <- c()
 if(file.exists("/home/t.wlodarczyk/chemical_ecology/sangchem/predicted_values.rds")){
   predicted_values <- readRDS("/home/t.wlodarczyk/chemical_ecology/sangchem/predicted_values.rds")
-  pred_values_sang <- predicted_values$pred_values_sang
-  pred_values_fusca <- predicted_values$pred_values_fusca
+  res_sang <- predicted_values$pred_values_sang
+  res_fusca <- predicted_values$pred_values_fusca
   seeds_sang <- seeds_sang
   seeds_fusca <- seeds_fusca
 }
@@ -35,7 +36,7 @@ predict_values <- function(n, model, sang_prop, seeds){
        rseed = .Random.seed)
 }
 pred_values_sang <- bplapply(1:1000, predict_values, model = lm_model, sang_prop = 1, seeds = seeds, BPPARAM = MulticoreParam())
-res_sang <- c()
+
 seeds_sang <- list()
 for(i in seq_along(pred_values_sang)){
   res_sang <- c(res_sang, pred_values_sang[[i]]$t)
@@ -45,7 +46,6 @@ for(i in seq_along(pred_values_sang)){
 model_input <- dev_samples[dev_samples$species=="F. fusca",]
 lm_model <- lmer(I(log(corrected_mass)) ~ sang_prop  + (1|colony), data=model_input)
 pred_values_fusca <- bplapply(1:1000, predict_values, model = lm_model, sang_prop = 0, seeds = seeds, BPPARAM = MulticoreParam())
-res_fusca <- c()
 seeds_fusca <- list()
 for(i in seq_along(pred_values_fusca)){
   res_fusca <- c(res_fusca, pred_values_fusca[[i]]$t)
