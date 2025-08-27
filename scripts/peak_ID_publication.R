e8d1019ee90817f54d9790c582ef5644e9c44cee
Tomasz Włodarczyk <tomwlo@gmail.com>
2025-08-27 23:24:15 +0200

    removal of redundant code

--- scripts/peak_ID_publication.R
+++ scripts/peak_ID_publication.R
@@ -4,8 +4,6 @@ setdiff(mass_spectra_data$peak_ID , batch7$peak_ID)
 peaks <- unique(batch7$peak_ID)
 mean_ret_times <- unlist(lapply(peaks, function(x) mean(batch7[batch7$peak_ID==x, "retention_time"])))
 peaks_ordered <- peaks[order(mean_ret_times)]
-p85_ind <- which(peaks_ordered==85)
-peaks_ordered <- c(peaks_ordered[1:p85_ind], 86, peaks_ordered[(p85_ind+1):length(peaks_ordered)])
 
 peak_ID_df <- data.frame(peak_ID = 1:length(peaks_ordered), old_ID = peaks_ordered)
 peak_ID_df$identification <- unlist(lapply(peaks_ordered, function(x) mass_spectra_data[mass_spectra_data$peak_ID==x,"identification"][1]))
