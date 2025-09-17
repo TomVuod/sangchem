f525824e6acd4116101cb7cbdaaf96cd018a0c6f
Tomasz Włodarczyk <tomwlo@gmail.com>
2025-09-17 20:14:08 +0200

    data recalculation on  University Computer Center

--- scripts/CHC_mass_difference.R
+++ scripts/CHC_mass_difference.R
@@ -1,4 +1,6 @@
 library(BiocParallel)
+library(dplyr)
+library(lme4)
 setwd("/home/t.wlodarczyk/chemical_ecology/sangchem")
 load("./data/development_data.rda")
 load("./data/mass_spectra_data.rda")
@@ -16,12 +18,12 @@ simulation_number=10^3
 predict_values <- function(n, model, sang_prop){
   exp(bootMer(model,function(x) predict(x,data.frame(sang_prop=sang_prop, head_width=1300),re.form=NA),nsim=simulation_number)$t)
 }
-pred_values_sang <- unlist(bplapply(1:1000, predict_values, model = lm_model, sang_prop = 1, seeds = seeds, BPPARAM = MulticoreParam(RNG=4381)))
+pred_values_sang <- unlist(bplapply(1:1000, predict_values, model = lm_model, sang_prop = 1, BPPARAM = MulticoreParam(RNG=4381)))
 
 model_input <- dev_samples[dev_samples$species=="F. fusca",]
 lm_model <- lmer(I(log(corrected_mass)) ~ sang_prop + I(head_width^2)  + (1|colony), data=model_input)
 pred_values_fusca <- unlist(bplapply(1:1000, predict_values, model = lm_model, sang_prop = 0, BPPARAM = MulticoreParam(RNG=4381)))
 
 
-saveRDS(list(pred_values_sang=res_sang, pred_values_fusca=res_fusca), "/home/t.wlodarczyk/chemical_ecology/sangchem/output/predicted_values.rds")
+saveRDS(list(pred_values_sang=pred_values_sang, pred_values_fusca=pred_values_fusca), "/home/t.wlodarczyk/chemical_ecology/sangchem/output/predicted_values.rds")
 
--- scripts/process_validation_data.R
+++ scripts/process_validation_data.R
@@ -1,6 +1,7 @@
 # process cross-validation on high-performance computing nodes
 # merge files with partial results into single ones
 
+setwd("/home/t.wlodarczyk/chemical_ecology/sangchem")
 AUC_basic <- c()
 for(i in 1:10) AUC_basic <- c(AUC_basic, readRDS(paste0("./output/true_labels_3_groups_", i, ".rds"))$AUC[1:100])
 
@@ -9,4 +10,7 @@ for(i in 1:10) AUC_random <- c(AUC_random, readRDS(paste0("./output/random_label
 
 
 discrimination_model_validation <- list(AUC_observed = AUC_basic, AUC_null = AUC_random)
-usethis::use_data(discrimination_model_validation)
+saveRDS(discrimination_model_validation, "./data/discrimination_model_validation.rda")
+
+CHC_mass_prediction <- readRDS("./output/predicted_values.rds")
+saveRDS(CHC_mass_prediction, "./data/CHC_mass_prediction.rda")
