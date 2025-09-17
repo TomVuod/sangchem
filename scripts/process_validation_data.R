80b02318eea9572d149c02e91467c08999db60ff
Tomasz Włodarczyk <tomwlo@gmail.com>
2025-09-17 13:10:53 +0200

    script update

--- scripts/process_validation_data.R
+++ scripts/process_validation_data.R
@@ -2,10 +2,10 @@
 # merge files with partial results into single ones
 
 AUC_basic <- c()
-for(i in 1:10) AUC_basic <- c(AUC_basic, readRDS(paste0("true_labels_3_groups_", i, ".rds"))$AUC[1:100])
+for(i in 1:10) AUC_basic <- c(AUC_basic, readRDS(paste0("./output/true_labels_3_groups_", i, ".rds"))$AUC[1:100])
 
 AUC_random <- c()
-for(i in 1:10) AUC_random <- c(AUC_random, readRDS(paste0("random_labels_3_groups_", i, ".rds"))$AUC[1:100])
+for(i in 1:10) AUC_random <- c(AUC_random, readRDS(paste0("./output/random_labels_3_groups_", i, ".rds"))$AUC[1:100])
 
 
 discrimination_model_validation <- list(AUC_observed = AUC_basic, AUC_null = AUC_random)
