7d36244fc34b41c9935802d3e81da15dbae7415f
Tomasz Włodarczyk <tomwlo@gmail.com>
2025-09-20 10:14:30 +0200

    data and globals update

--- scripts/Body_area.R
+++ scripts/Body_area.R
@@ -4,6 +4,9 @@ body_surface_area[body_surface_area$ID==389,"head_width"] = 946 # measured again
 # sample 356 should be F. fusca but is F. sanguinea so it is mislabeled
 body_surface_area <- body_surface_area[body_surface_area$ID!=356,]
 body_surface_area <- body_surface_area[,colnames(body_surface_area)!="X"]
+body_surface_area$view[body_surface_area$view=="top"] = "Thorax dorsal view"
+body_surface_area$view[body_surface_area$view=="side"] = "Thorax lateral view"
+body_surface_area$view[body_surface_area$view=="head"] = "Head dorsal view"
 usethis::use_data(body_surface_area, overwrite = TRUE)
 
 
